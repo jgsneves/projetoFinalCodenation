095ff22d746816b9c74d3465f0e5a6c77e9e493d
jgsneves <jgsn.adv@gmail.com>
2020-07-16 12:04:28 -0300

    endpoint get users created

--- projectAPI/api/views.py
+++ projectAPI/api/views.py
@@ -1,5 +1,14 @@
-from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import authentication, permissions
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes
+from django.contrib.auth.models import User
+from .serializers import UserSerializer
 
 # Create your views here.
+
+@api_view()
+@permission_classes([IsAuthenticated])
+def getUsers(request):
+    queryset = User.objects.all()
+    serializer = UserSerializer(queryset, many=True)
+    return Response(serializer.data)
