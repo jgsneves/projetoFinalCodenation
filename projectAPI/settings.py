897f2196f88643155a4d06c30576642d84fd4c38
jgsneves <jgsn.adv@gmail.com>
2020-07-19 12:18:34 -0300

    humanized datetime

--- projectAPI/settings.py
+++ projectAPI/settings.py
@@ -51,6 +51,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
+    'DATETIME_FORMAT': "%Y-%m-%d",
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
 }
